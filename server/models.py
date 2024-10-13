f7633425496b70e312ec3f767f41ecc0c59ea16d
aggreyrc <aggreyronoh@gmail.com>
2024-10-13 16:42:33 +0300

    Created models

---
+++ server/models.py
@@ -0,0 +1,67 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData, CheckConstraint
+from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.ext.associationproxy import association_proxy
+
+
+
+
+db = SQLAlchemy()
+
+# Episodes Table
+class Episode(db.Model, SerializerMixin):
+    __tablename__ = "episodes"
+    
+    # serialize rules
+    serialize_rules = ('-appearances.episode')
+    
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String, nullable=False)
+    number = db.Column(db.Integer, nullable=False)
+    
+    # Relationship to Appearance model
+    appearances = db.relationship('Appearance', back_populates='episode', cascade="all,delete-orphan")
+    
+    def __repr__(self):
+        return f'<Episode {self.date} for {self.number}>'
+    
+    
+# Guests Table
+class Guest(db.Model, SerializerMixin):
+    __tablename__ = "guests"
+    
+    # serialize rules
+    serialize_rules = ('-appearances.guest')
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    occupation = db.Column(db.String, nullable=False)
+    
+    # Relationship to Appearances model
+    appearances = db.relationship('Appearance', back_populates='guest', cascade="all,delete-orphan")
+    
+    def __repr__(self):
+        return f'<Guest {self.name} for {self.occupation}>'
+    
+    
+# Appearances Table
+class Appearance(db.Model, SerializerMixin):
+    __tablename__ = "appearances"
+    
+    # serialize rules
+    serialize_rules = ('-episode.appearances', '-guest.appearances')
+    
+    id = db.Column(db.Integer, primary_key=True)
+    rating = db.Column(db.Integer, nullable=False)
+    episode_id = db.Column(db.Integer, db.ForeignKey('episodes.id'), nullable=False)
+    guest_id = db.Column(db.Integer, db.ForeignKey('guests.id'), nullable=False)
+    
+    # Relationship to Episode and Guest models
+    episode = db.relationship('Episode', back_populates='appearances')
+    guest = db.relationship('Guest', back_populates='appearances')
+    
+    # Validating rating column
+    __table_args__ = (CheckConstraint('rating >= 1 AND rating <= 5', name='valid_rating'),)
+    
+    def __repr__(self):
+        return f'<Appearance of {self.guest.name} in {self.episode.date} for {self.episode.number}>'
